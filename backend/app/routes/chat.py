d3c9734b028ff3637d7e8ff8a6c3c1b61dabfef4
crbazevedo <carlos.azevedo@bain.com>
2023-09-23 20:51:33 -0300

    add gpt-4 for move explanations

---
+++ backend/app/routes/chat.py
@@ -0,0 +1,14 @@
+import openai
+
+openai.api_key = "my_openai_key"
+
+def get_move_explanation(move, fen):
+    prompt = f"Explain the chess move {move} in the position {fen}."
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=prompt,
+        max_tokens=50
+    )
+    explanation = response.choices[0].text.strip()
+    return explanation
+
